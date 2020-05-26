c3dd56c76ce575dc3e920352edc125a342a9f4ae
Michele <morticcino@diegoli.org>
2020-05-26 08:36:20 +0200

    Update F3D.vue

--- src/F3D.vue
+++ src/F3D.vue
@@ -5,8 +5,8 @@
         <div v-on:click="fn_hideShowToolbar">
           <svg
             class="bi bi-justify"
-            width="1em"
-            height="1em"
+            width="1.5em"
+            height="1.5em"
             viewBox="0 0 16 16"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
